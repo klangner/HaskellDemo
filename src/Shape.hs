a8b4f9a9232c8621a538e0769995f86ac7d80181
Krzysztof Langner <klangner@gmail.com>
2013-09-17 18:17:33 +0200

    Polymorphic list

--- src/Shape.hs
+++ src/Shape.hs
@@ -2,21 +2,29 @@ module Shape where
 
 class Shape a where
     move :: a -> Float -> Float -> a
-    scale :: a -> Float -> a
+    resize :: a -> Float -> a
 
 -- Base types
-type Name = String
 data Position = Position Float Float deriving (Show)
 data Size = Size Float Float deriving (Show)
 
--- Shapes
-data Rectangle = Rectangle Name Position Size deriving (Show)
+-- Rectangle
+data Rectangle = Rectangle Position Size deriving (Show)
 instance Shape Rectangle where
-    move (Rectangle name (Position x y) size) dx dy = Rectangle name (Position (x+dx) (y+dy)) size
-    scale (Rectangle name pos (Size w h)) scale = Rectangle name pos (Size (w*scale) (h*scale))
-    
-data Circle = Circle  Name Position Float deriving (Show)
-instance Shape Circle where
-    move (Circle name (Position x y) r) dx dy = Circle name (Position (x+dx) (y+dy)) r
-    scale (Circle name pos r) scale = Circle name pos (r*scale)
+    move (Rectangle (Position x y) size) dx dy = Rectangle (Position (x+dx) (y+dy)) size
+    resize (Rectangle pos (Size w h)) scale = Rectangle pos (Size (w*scale) (h*scale))
 
+-- Circle
+data Circle = Circle Position Float deriving (Show)
+instance Shape Circle where
+    move (Circle (Position x y) r) dx dy = Circle (Position (x+dx) (y+dy)) r
+    resize (Circle pos r) scale = Circle pos (r*scale)
+
+{-
+-- Curred actions
+moveAction :: Shape a => Float -> Float -> (a -> a)
+moveAction dx dy = \s -> move s dx dy
+
+resizeAction :: Shape a => Float -> (a -> a)
+resizeAction factor = \s -> resize s factor
+-}
--- src/Testbed.hs
+++ src/Testbed.hs
@@ -1,11 +1,36 @@
 module Testbed where
 
+import qualified Data.Map as Map
 import Shape
 
+-- Types
+type Name = String
+data MyShape = MyRect Name Rectangle
+             | MyCircle Name Circle
+                deriving (Show)
+
 -- Init sample model
-rect1 = Rectangle "rect" (Position 10 10) (Size 50 50)
-model = [rect1]
+model :: [MyShape]
+model = [
+        MyRect "r1" (Rectangle (Position 10 10) (Size 50 50)),
+        MyCircle "c1" (Circle (Position 20 30) 5)
+        ]
 
 -- Same actions
---move10 :: (Shape a) -> (Shape a)
-move10 shape = move shape 10 10
+moveAction :: Float -> Float -> (MyShape -> MyShape)
+moveAction dx dy = \s -> case s of
+                    MyRect name rect -> MyRect name (move rect dx dy)
+                    MyCircle name circle -> MyCircle name (move circle dx dy)
+
+resizeAction :: Float -> (MyShape -> MyShape)
+resizeAction factor = \s -> case s of
+                    MyRect name rect -> MyRect name (resize rect factor)
+                    MyCircle name circle -> MyCircle name (resize circle factor)
+
+
+actions :: [(Name, MyShape -> MyShape)]
+actions = [
+            ("rect", moveAction 10 10), 
+            ("c1", moveAction 1 4)
+          ]
+
