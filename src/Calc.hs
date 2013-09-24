a41f7ac2e2128461f657c960c44a3f56a7d65568
Krzysztof Langner <klangner@gmail.com>
2013-09-24 23:01:35 +0200

    Working parser

--- src/Calc.hs
+++ src/Calc.hs
@@ -1,63 +1,113 @@
-{- Calculator -}
 module Calc where
 
-type Parser a = String -> [(a, String)]
+import Data.Char
+import Control.Monad
 
-{-
-Grammar:
-  * Term = Product '+' Product
-  * Product = Expression '*' Expression
-  * Expression = '(' Term ')' | Val
-  * Val = Float
--}
-data Element = Term 
-             | Product
-             | Expression
-             | Val
 
--- Parse single character
-item :: Parser Char
-item cs = case cs of
-                [] -> []
-                (x:xs) -> [(x, xs)]
-
--- Always fail
-failure :: Parser a
-failure = \inp -> []
-
--- Return v without consuming input
-return :: a -> Parser a
-return v = \inp -> [(v, inp)]
+-- To samo co:
+-- data Parser a = P (String -> [(a, String)])
+newtype Parser a = P (String -> [(a, String)])
 
 -- Sequencing parser
-(>>=) :: Parser a -> (a -> Parser b) -> Parser b
-p >>= f = \inp -> case parse p inp of
+instance Monad Parser where
+    return v = P (\inp -> [(v, inp)])
+    p >>= f = P (\inp -> case parse p inp of
                         [] -> []
-                        [(v,rest)] -> parse (f v) rest
+                        [(v,rest)] -> parse (f v) rest)
 
--- Return first if parsed input
-(+++) :: Parser a -> Parser a -> Parser a
-p +++ q = \inp -> case p inp of
-            [] -> q inp
-            [(c, rest)] -> [(c, rest)]
+instance MonadPlus Parser where
+    mzero =  P (\inp -> [])
+    p `mplus` q =  P (\inp -> case parse p inp of
+                               []        -> parse q inp
+                               [(v,out)] -> [(v,out)])
 
 -- Apply parser
 parse :: Parser a -> String -> [(a, String)]
-parse p inp = p inp
-
---p :: Parser (Char,Char)
-p = do x <- item
-       z <- item
-       y <- item
-       return (x, y)
+parse (P p) inp = p inp
 
 
-p1 = do x <- item
-        return x
+-- Always fail
+failure :: Parser a
+failure = mzero
 
-p2 = do x <- item
-        return2 x
 
-p3 = item
-     
+-- Parse single character
+item :: Parser Char
+item = P (\inp -> case inp of
+                [] -> []
+                (x:xs) -> [(x, xs)])
 
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q =  p `mplus` q
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do x <- item
+           if p x then return x else failure
+
+digit :: Parser Char
+digit = sat isDigit
+lower :: Parser Char
+lower =  sat isLower
+upper :: Parser Char
+upper =  sat isUpper
+letter :: Parser Char
+letter =  sat isAlpha
+alphanum :: Parser Char
+alphanum =  sat isAlphaNum
+char :: Char -> Parser Char
+char x =  sat (== x)
+
+string :: String -> Parser String
+string [] = return []
+string (x:xs) = do char x
+                   string xs
+                   return (x:xs) 
+
+-- Apply parser 0 or more times
+many :: Parser a -> Parser [a]
+many p = many1 p +++ return []
+
+-- Apply at least once
+many1 :: Parser a -> Parser [a]
+many1 p = do v <- p
+             vs <- many p
+             return (v:vs)
+
+-- Identifier
+ident :: Parser String
+ident = do v  <- lower
+           vs <- many alphanum
+           return (v:vs)
+
+-- Natural numbers
+nat :: Parser Int
+nat = do vs <- many1 digit
+         return (read vs)
+
+space :: Parser () -- Empty tuple
+space = do vs <- many (sat isSpace)
+           return ()
+
+token :: Parser a -> Parser a
+token p = do space
+             v <- p
+             space
+             return v
+
+-- Strip spaces
+identifier :: Parser String
+identifier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+symbol :: String -> Parser String
+symbol xs = token (string xs)
+
+p :: Parser [Int]
+p = do symbol "["
+       n <- natural
+       ns <- many (do symbol ","
+                      natural)
+       symbol "]"
+       return (n:ns)  
--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -1,30 +1,6 @@
+{-# LANGUAGE NPlusKPatterns #-}
 module Sandbox where
 
-import Data.Char
+import Control.Monad
 
-let2int :: Char -> Int
-let2int c = ord c - ord 'a'
-
-int2let :: Int -> Char
-int2let x = chr (x + ord 'a')
-
-shift :: Int -> Char -> Char
-shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
-          | otherwise = c
-
-encode :: Int -> String -> String
-encode n xs = [shift n x | x <- xs]
-
-percent :: Int -> Int -> Float
-percent n m = (fromIntegral n / fromIntegral m) * 100
-
--- Count number of lower letters
-lowers :: String -> Int
-lowers xs = length [x | x <- xs, isLower x]
-
-count :: Char -> String -> Int
-count c xs = length [x | x <- xs, x == c]
-
---freqs :: String -> [Float]
---freqs xs = []
-  --          where n 
+type Parser a              =  P (String -> [(a,String)])
