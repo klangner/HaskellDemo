79a8ac75e3fa5f2db4c50a02f6ddd7a980c1378b
Krzysztof Langner <klangner@gmail.com>
2013-10-23 12:54:19 +0200

    list to functions

--- src/dsp/Basic.hs
+++ src/dsp/Basic.hs
@@ -1,30 +1,25 @@
 module Basic where
 
 type Signal = [Float]
-
--- Add 2 signals
-add :: Signal -> Signal -> Signal
-add [] _ = []
-add _ [] = []
-add (a:as) (b:bs) = (a+b:add as bs)
-
--- Delay signal
-delay :: Signal -> Integer -> Signal
-delay [] _ = []
-delay as 0 = as
-delay as n = (0: delay as (n-1))
+type Input = Integer -> Float
 
 -- Scale signal
-scale :: Signal -> Float -> Signal
-scale [] _ = []
-scale (a:as) f = (a*f:scale as f)
+scale :: Float -> Input -> Integer -> Float
+scale a input n = a*(input n)
+
+-- Add signals
+add :: Input -> Input -> Integer -> Float
+add a b n = (a n) + (b n)
 
 
--- Test data
-impulse :: [Float]
-impulse = [0, 0, 0, 1, 0, 0, 0]
-s2 :: [Float]
-s2 = [1, 2, 3, 4, 5, 6]
-step :: [Float]
-step = [0, 0, 0, 1, 1, 1, 1]
+-- Delta (impulse) function
+delta :: Integer -> Float
+delta 0 = 1
+delta _ = 0
+
+-- Delta (impulse) function
+step :: Integer -> Float
+step n
+   | n > 0 = 1
+   | otherwise = 0
 
