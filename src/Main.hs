654d2a72f5804c0823f613fb9edb963f5004ecab
Krzysztof Langner <klangner@gmail.com>
2014-03-31 13:54:50 +0200

    Working countdown

--- src/Main.hs
+++ src/Main.hs
@@ -1,39 +1,7 @@
 
-import qualified Data.Map as Map
-import Old.Shape
-
--- Init sample model
-baseModel :: Model
-baseModel = [
-        Rectangle "rect1" (Position 10 10) (Size 50 50),
-        Circle "c1" (Position 20 30) 5
-        ]
-
-keyBindings :: Map.Map Char Action
-keyBindings =  Map.fromList [
-            ('w', moveAction 10 0), 
-            ('s', moveAction (-10) 0),
-            ('a', moveAction 0 (-10)),
-            ('d', moveAction 0 10)
-          ]
-
-key2action :: Char -> Maybe Action
-key2action c = Map.lookup c keyBindings
-
-
-actionLoop :: Maybe Action -> Model -> IO ()
-actionLoop Nothing _ = return ()
-actionLoop (Just action) model = do 
-    print newModel
-    c <- getChar
-    actionLoop (key2action c) newModel
-    where
-        newModel = map action model
-        
-
+import PIH.Countdown
 
 main :: IO ()
 main = do
-    c <- getChar
-    actionLoop (key2action c) baseModel
+    demo1
 
--- src/PIH/Countdown.hs
+++ src/PIH/Countdown.hs
@@ -1,12 +1,15 @@
 module PIH.Countdown where
 
 import Control.Applicative
+import Data.List
+
 
 data Expr = Num Int
           | Plus Expr Expr
           | Minus Expr Expr
           | Multi Expr Expr
           | Div Expr Expr
+          deriving (Eq)
           
 instance Show Expr where
     show (Num value) = show value
@@ -18,7 +21,8 @@ instance Show Expr where
 
 -- Solve countdown game
 solve :: [Int] -> Int -> [Expr]
-solve _ _ = []
+solve xs n = filter p $ concatMap build (permute xs)
+    where p e = eval e == [n]
 
 
 -- Evaluate data. 
@@ -30,18 +34,44 @@ eval (Minus a b) = case (-) <$> (eval a) <*> (eval b) of
                     [x] -> if x >= 0 then pure x else []
                     _ -> []
 eval (Multi a b) = (*) <$> (eval a) <*> (eval b)
-eval (Div a b) = case mod <$> x <*> y of           
-                    [0] -> div <$> x <*> y
+eval (Div a b) = case mod <$> x <*> y' of           
+                    [0] -> div <$> x <*> y'
                     _ -> []
     where x = eval a
           y = eval b
+          y' = if y == [0] then [] else y
 
+-- Check if given expression is valid
+valid :: Expr -> Bool
+valid expr = eval expr /= [] 
+
+-- Combine 2 expressions
+combine :: Expr -> Expr ->[Expr]
+combine e1 e2 = filter valid [Plus e1 e2, Minus e1 e2, Multi e1 e2, Div e1 e2]
+--combine e1 e2 = [Plus e1 e2, Minus e1 e2, Multi e1 e2, Div e1 e2]
+
+-- Build all possible expressions from given numbers without changing order
+build :: [Int] -> [Expr]
+build [] = []
+build [x] = [Num x]
+build xs = concatMap f (split xs)
+    where f (a, b) = concat [ combine exp1 exp2 | exp1 <- build a, exp2 <- build b]
+
+-- get all permutation with all lengths
+permute :: [a] -> [[a]]
+permute xs = concatMap permutations (subsequences xs)
+
+-- Split array at any position
+split :: [Int] -> [([Int],[Int])]
+split [x] = [([x],[])]
+split xs = map (\x -> splitAt x xs) [1 .. (length xs - 1)]
 
 -- ----------------------------------------------------------------------------
 -- Demo
 -- ----------------------------------------------------------------------------
 
+-- 780 solutions
 demo1 :: IO ()
 demo1 = do
     let solution = solve [1,3,7,10,25,50] 765
-    print solution
+    print (length solution)
