c7de687a42e753197c831c8db5a17b69d08dddce
Krzysztof Langner <klangner@gmail.com>
2014-03-31 14:29:35 +0200

    Optimized countdown. Evaluates and filters expression during generation.

--- src/PIH/Countdown.hs
+++ src/PIH/Countdown.hs
@@ -1,59 +1,43 @@
 module PIH.Countdown where
 
-import Control.Applicative
 import Data.List
 
 
-data Expr = Num Int
-          | Plus Expr Expr
-          | Minus Expr Expr
-          | Multi Expr Expr
-          | Div Expr Expr
-          deriving (Eq)
+data Op = Plus | Minus | Multi | Div deriving( Show )
+data Expr = Val Int | App Op Expr Expr deriving( Show )
           
-instance Show Expr where
-    show (Num value) = show value
-    show (Plus a b) = "(" ++ show a ++ "+" ++ show b ++ ")"
-    show (Minus a b) = "(" ++ show a ++ "-" ++ show b ++ ")"
-    show (Multi a b) = show a ++ "*" ++ show b
-    show (Div a b) = show a ++ "/" ++ show b           
-
+type Result = (Expr, Int)          
+          
 
 -- Solve countdown game
-solve :: [Int] -> Int -> [Expr]
+solve :: [Int] -> Int -> [Result]
 solve xs n = filter p $ concatMap build (permute xs)
-    where p e = eval e == [n]
-
-
--- Evaluate data. 
--- At every step the result has to be positive natural number
-eval :: Expr -> [Int]
-eval (Num value) = pure value
-eval (Plus a b) = (+) <$> (eval a) <*> (eval b)
-eval (Minus a b) = case (-) <$> (eval a) <*> (eval b) of
-                    [x] -> if x >= 0 then pure x else []
-                    _ -> []
-eval (Multi a b) = (*) <$> (eval a) <*> (eval b)
-eval (Div a b) = case mod <$> x <*> y' of           
-                    [0] -> div <$> x <*> y'
-                    _ -> []
-    where x = eval a
-          y = eval b
-          y' = if y == [0] then [] else y
+    where p (_,v) = v == n
 
 -- Check if given expression is valid
-valid :: Expr -> Bool
-valid expr = eval expr /= [] 
+valid :: Op -> Int -> Int -> Bool
+valid Plus _ _  = True
+valid  Minus a b = a > b 
+valid  Multi _ _ = True
+valid Div a b = a `mod` b == 0           
+
+-- Apply operation
+apply :: Op -> Int -> Int -> Int
+apply Plus a b  = a+b
+apply  Minus a b = a - b 
+apply  Multi a b = a*b
+apply Div a b = a `div` b           
+
 
 -- Combine 2 expressions
-combine :: Expr -> Expr ->[Expr]
-combine e1 e2 = filter valid [Plus e1 e2, Minus e1 e2, Multi e1 e2, Div e1 e2]
---combine e1 e2 = [Plus e1 e2, Minus e1 e2, Multi e1 e2, Div e1 e2]
+combine :: Result -> Result ->[Result]
+combine (e1, v1) (e2, v2) = [(App op e1 e2, apply op v1 v2) | op <- [Plus, Minus, Multi, Div], valid op v1 v2 ]
+
 
 -- Build all possible expressions from given numbers without changing order
-build :: [Int] -> [Expr]
+build :: [Int] -> [Result]
 build [] = []
-build [x] = [Num x]
+build [x] = [(Val x, x)]
 build xs = concatMap f (split xs)
     where f (a, b) = concat [ combine exp1 exp2 | exp1 <- build a, exp2 <- build b]
 
