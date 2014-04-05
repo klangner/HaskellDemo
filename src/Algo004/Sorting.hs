c1849be4740fbd186dcc7779973e815cf6b036d7
Krzysztof Langner <klangner@gmail.com>
2014-04-05 11:01:00 +0200

    Algo004 exercises

---
+++ src/Algo004/Sorting.hs
@@ -0,0 +1,16 @@
+module Algo004.Sorting where
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = mergeSorted (msort kx) (msort lx)
+    where b = (length xs) `div` 2
+          (kx, lx) = splitAt b xs
+
+-- | join 2 sorted array in one also sorted
+mergeSorted :: Ord a => [a] -> [a] -> [a]
+mergeSorted xs ys | null xs = ys
+                  | null ys = xs
+                  | (head xs) < (head ys) = (head xs: mergeSorted (tail xs) ys)
+                  | otherwise = (head ys: mergeSorted xs (tail ys)) 
+
